c687ecfa9ea191b62e505270e10310a6645bd863
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-03-23 19:24:05 +0700

    feat: add `next-pwa` config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
+const withPWA = require("next-pwa");
 
 const nextConfig = {
   webpack: (config, options) => {
@@ -17,6 +18,13 @@ const nextConfig = {
   },
 };
 
-const nextPlugins = [optimizedImages];
+const nextPlugins = [
+  optimizedImages,
+  withPWA({
+    pwa: {
+      dest: "public",
+    },
+  }),
+];
 
 module.exports = withPlugins([nextPlugins], nextConfig);
