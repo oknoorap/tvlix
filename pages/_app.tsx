562a3387dee92bf287e55777d0e6a079bba98200
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-03-24 09:45:56 +0700

    feat: add meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import defaultTheme from "themes/default";
@@ -5,6 +6,12 @@ import defaultTheme from "themes/default";
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={defaultTheme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
