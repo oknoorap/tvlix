9a09fc0c7ab1323654f1f290d9562d948231254a
Ribhararnus Pracutian <oknoorap@gmail.com>
2021-03-24 14:19:23 +0700

    feat: add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_WEBSITE_URL,
+  sitemapSize: 5000,
+  changefreq: "monthly",
+  priority: 0.7,
+  generateRobotsTxt: true,
+  outDir: "out",
+};
